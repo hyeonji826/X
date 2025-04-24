0617a28d2a1ee6e62b64abcf1fe14d400d6463d6
노현지 <shguswl826@gmail.com>
2025-04-24 15:24:04 +0900

    배열을 이용한 auth 커밋

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,13 +1,16 @@
 // 사용자
 import express from "express"
+import * as authController from "../controller/auth.mjs"
 
 const router = express.Router()
 
 // 회원가입
-
+// POST
+router.post('/signup',authController.signup)
 
 // 로그인
-
+// POST
+router.post('/login',authController.login)
 
 // 로그인 유지
 
